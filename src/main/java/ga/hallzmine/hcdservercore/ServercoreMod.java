baeb6bf0223fdb1ce6a5f432f8239fb14d9931cc
Hallzmine <dakehall@gmail.com>
2021-05-20 07:44:22 -0400

    Cleaned up code and debranded

--- src/main/java/ga/hallzmine/hcdservercore/ServercoreMod.java
+++ src/main/java/ga/hallzmine/hcdservercore/ServercoreMod.java
@@ -1,25 +1,16 @@
 package ga.hallzmine.hcdservercore;
 
-import net.minecraft.block.Block;
-import net.minecraft.block.Blocks;
-import net.minecraft.client.renderer.entity.PlayerRenderer;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.common.MinecraftForge;;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
 import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-
-import java.util.stream.Collectors;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod("hcdservercore")
@@ -50,7 +41,7 @@ public class ServercoreMod
     // You can use SubscribeEvent and let the Event Bus discover methods to call
     @SubscribeEvent
     public void onServerStarting(FMLServerStartingEvent event) {
-        LOGGER.info("This server is running Hallzmine's Crafting Dead Server Core!");
+        LOGGER.info("This server is running Crafting Dead Loot!");
     }
 
     public static final ItemGroup TAB = new ItemGroup("hcdservercoreTab") {
